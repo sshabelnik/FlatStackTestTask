e6e94b668646f1dfdcf4c5a2c7effdb9ae4e67ca
Sergey Shabelnik <s.shabelnik@agggro.com>
2021-06-13 16:49:49 +0300

    Initial commit

---
+++ FlatStackTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatStackTestTask
+//
+//  Created by Сергей Шабельник on 13.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
